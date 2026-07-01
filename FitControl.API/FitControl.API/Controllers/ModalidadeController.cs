33cf3989ee348aff47d50698fe238497966f8e0b
nunolpt <nunolomba19@gmail.com>
2026-07-01 18:53:08 +0100

    Adicionado UserController.cs , ModalidadeController.cs e UserDto.cs

---
+++ FitControl.API/FitControl.API/Controllers/ModalidadeController.cs
@@ -0,0 +1,143 @@
+﻿using FitControl.API.Data;
+using FitControl.API.Entities;
+using FitControl.API.Models;
+using Mapster;
+using MapsterMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitControl.API.Controllers;
+
+public class ModalidadeController : ControllerBase
+{
+    private readonly IFitControlDbContext _fitControlDbContext;
+    private readonly IMapper _mapper;
+    
+    public ModalidadeController(IFitControlDbContext fitControlDbContext, IMapper mapper)
+    {
+        _fitControlDbContext = fitControlDbContext;
+        _mapper = mapper;
+    }
+    
+    [HttpGet("/modalidades")]
+    public async Task<List<Modalidade>> GetModalidades()
+    {
+        if (_fitControlDbContext.Modalidades is not null)
+        {
+            var modalidades = _fitControlDbContext.Modalidades
+                .Include(x => x.NivelDificuldade)
+                .Where(m => m.IsDeleted == false && m.NivelDificuldade.IsDeleted == false);
+            
+            if (modalidades.Any())
+            {
+                return await modalidades.ToListAsync();
+            }
+        }
+
+        return new List<Modalidade>();
+    }
+    
+    [HttpGet("/modalidade/{id}")]
+    public async Task<Modalidade> GetModalidade(int id)
+    {
+        if (_fitControlDbContext.Modalidades is not null)
+        {
+            var modalidade = await _fitControlDbContext.Modalidades
+                .Include(x => x.NivelDificuldade)
+                .FirstOrDefaultAsync(m => (m.IsDeleted == false || m.NivelDificuldade.IsDeleted == false) && m.Id == id);
+            
+            if (modalidade is not null)
+            {
+                return modalidade;
+            }
+        }
+        return new Modalidade();
+    }
+    
+    [HttpPost("modalidade")]
+    public async Task<IResult> AddModalidade([FromBody] ModalidadeDto? modalidade)
+    {
+        if (modalidade is null)
+        {
+            return Results.BadRequest();   
+        }
+        
+        modalidade.NivelDificuldade = null;
+        
+        var mapper = _mapper.Map<Models.ModalidadeDto, Entities.Modalidade>(modalidade);
+
+        mapper.CreatedAt = DateTime.Now;
+        mapper.UpdatedAt = DateTime.Now;
+        
+        var modalidades = _fitControlDbContext.Modalidades;
+
+        if (modalidades is not null)
+        {
+            modalidades.Add(mapper);
+            await _fitControlDbContext.SaveChangesAsync();
+            return Results.Ok("Modalidade adicionada com sucesso!");
+        }
+        return Results.Empty;
+    }
+    
+    [HttpPut("/modalidade")]
+    public async Task<IActionResult> UpdateModalidade ([FromBody] ModalidadeDto? modalidadeDto)
+    {
+        if (modalidadeDto is null)
+        {
+            return BadRequest();
+        }
+        
+        modalidadeDto.NivelDificuldade = null;
+
+        if (_fitControlDbContext.Modalidades is null)
+        {
+            return NotFound();
+        }
+        
+        var oldModalidade = await _fitControlDbContext.Modalidades.FirstOrDefaultAsync(m => m.Id == modalidadeDto.Id);
+
+        if (oldModalidade is null)
+        {
+            return NotFound("Modalidade não foi encontrada!");
+        }
+
+        modalidadeDto.Adapt(oldModalidade);
+        
+        try
+        {
+            var result = await _fitControlDbContext.SaveChangesAsync();
+
+            if (result <= 0)
+            {
+                return NotFound("Não foi possível guardar os dados");
+            }
+        }
+        catch (Exception e)
+        {
+            return NotFound(e.Message);
+        }
+
+        return Ok(modalidadeDto);
+    }
+    
+    [HttpDelete("/modalidade/softdelete/{id}")]
+    public async Task<IResult> SoftDeleteModalidade(int id)
+    {
+        if (_fitControlDbContext.Modalidades is not null)
+        {
+            var modalidade = await _fitControlDbContext.Modalidades.FirstOrDefaultAsync(m => m.Id == id);
+            if (modalidade is null)
+            {
+                return Results.NotFound("Modalidade não encontrada");
+            }
+            
+            modalidade.IsDeleted = true;
+            modalidade.UpdatedAt = DateTime.Now;
+            
+            await _fitControlDbContext.SaveChangesAsync();
+            return Results.Ok("Modalidade apagada com sucesso!");
+        }
+        return Results.Empty; 
+    }
+}
---
+++ FitControl.API/FitControl.API/Controllers/UserController.cs
@@ -0,0 +1,136 @@
+﻿using FitControl.API.Data;
+using FitControl.API.Entities;
+using FitControl.API.Models;
+using Mapster;
+using MapsterMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitControl.API.Controllers;
+
+public class UserController : ControllerBase
+{
+    private readonly IFitControlDbContext _fitControlDbContext;
+    private readonly IMapper _mapper;
+    
+    public UserController(IFitControlDbContext fitControlDbContext, IMapper mapper)
+    {
+        _fitControlDbContext = fitControlDbContext;
+        _mapper = mapper;
+    }
+    
+    [HttpGet("/users")]
+    public async Task<List<User>> GetUsers()
+    {
+        if (_fitControlDbContext.Users is not null)
+        {
+            var users = _fitControlDbContext.Users
+                .Where(u => u.IsDeleted ==  false);
+            
+            if (users.Any())
+            {
+                return await users.ToListAsync();
+            }
+        }
+
+        return new List<User>();
+    }
+    
+    [HttpGet("/user")]
+    public async Task<IActionResult> GetUser([FromBody] UserDto userDto)
+    {
+        if (_fitControlDbContext.Users != null)
+        {
+            var checkUser = await _fitControlDbContext.Users.AnyAsync(x => x.Username.Equals(userDto.Username) && x.Password == userDto.Password);
+
+            if (checkUser)
+            {
+                return Ok(checkUser);
+            }
+        }
+        return NotFound("Username or password is incorrect");
+    }
+    
+    [HttpPost("user")]
+    public async Task<IResult> AddUser([FromBody] UserDto? user)
+    {
+        if (user is null)
+        {
+            return Results.BadRequest();   
+        }
+        
+        var mapper = _mapper.Map<Models.UserDto, Entities.User>(user);
+
+        mapper.CreatedAt = DateTime.Now;
+        mapper.UpdatedAt = DateTime.Now;
+        
+        var users = _fitControlDbContext.Users;
+
+        if (users is not null)
+        {
+            users.Add(mapper);
+            await _fitControlDbContext.SaveChangesAsync();
+            return Results.Ok("User adicionado com sucesso!");
+        }
+        return Results.Empty;
+    }
+    
+    [HttpPut("/user")]
+    public async Task<IActionResult> UpdateUser ([FromBody] UserDto? userDto)
+    {
+        if (userDto is null)
+        {
+            return BadRequest();
+        }
+
+        if (_fitControlDbContext.Users is null)
+        {
+            return NotFound();
+        }
+        
+        var oldUser = await _fitControlDbContext.Users.FirstOrDefaultAsync(u => u.Id == userDto.Id);
+
+        if (oldUser is null)
+        {
+            return NotFound("User não foi encontrado!");
+        }
+
+        userDto.Adapt(oldUser);
+        
+        try
+        {
+            var result = await _fitControlDbContext.SaveChangesAsync();
+
+            if (result <= 0)
+            {
+                return NotFound("Não foi possível guardar os dados");
+            }
+        }
+        catch (Exception e)
+        {
+            return NotFound(e.Message);
+        }
+
+        return Ok(userDto);
+    }
+    
+    [HttpDelete("/user/softdelete/{id}")]
+    public async Task<IResult> SoftDeleteUser(int id)
+    {
+        if (_fitControlDbContext.Users is not null)
+        {
+            var user = await _fitControlDbContext.Users.FirstOrDefaultAsync(u => u.Id == id);
+            if (user is null)
+            {
+                return Results.NotFound("User não encontrado");
+            }
+            
+            user.IsDeleted = true;
+            user.UpdatedAt = DateTime.Now;
+            
+            await _fitControlDbContext.SaveChangesAsync();
+            return Results.Ok("User apagado com sucesso!");
+        }
+        return Results.Empty; 
+    }
+}
